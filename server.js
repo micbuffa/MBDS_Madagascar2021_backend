f20a4e45f7514e9c4843c42cd65ae83ea20a7501
micbuffa <micbuffa@gmail.com>
2021-03-10 15:49:21 +0100

    première  mise à jour

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.route(prefix + '/assignments/:id')
 
 // On démarre le serveur
 app.listen(port, "0.0.0.0");
-console.log('Serveur démarré sur http://localhost:' + port);
+console.log('Serveur démarré  sur http://localhost:' + port);
 
 module.exports = app;
 
